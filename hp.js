461661bea084319e374af8b5ef478e0d64df20f8
mkrtchyan13 <jane.mkrtchyan@mail.ru>
2017-09-12 22:50:25 +0400

    first commit

---
+++ hp.js
@@ -0,0 +1,16 @@
+alert("It's leviosa not leviosa");
+
+const harry = 21;
+const ron = 2;
+ if (harry - 2 > ron + 10) {
+ 	console.log("Gryffindor")
+ }
+ else {
+ 	console.log("Slytherin")
+ }
+
+const quidich = 25;
+const malfoy = 22 ;
+
+console.log (quidich + malfoy);
+console.log(malfoy/quidich);
